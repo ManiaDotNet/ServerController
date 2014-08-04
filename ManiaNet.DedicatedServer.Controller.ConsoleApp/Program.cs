debc3f0cc267ea8d432f4b2b8b93ce3bba0e0c56
Arne Kiesewetter <banane1996@gmx.net>
2014-08-04 23:23:38 +0200

    Fix naming.

--- ManiaNet.DedicatedServer.Controller.ConsoleApp/Program.cs
+++ ManiaNet.DedicatedServer.Controller.ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
-namespace ManiaNet.ConsoleTesting
+namespace ManiaNet.DedicatedServer.Controller.ConsoleApp
 {
     internal class Program
     {
